a9b7827b2b90405f0ca792ab9fdd1ffa385604dc
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-02-07 17:18:14 +0000

    Output an additional type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { DotPropPaths, DotPropPathsRecord, DotPropPathsUnion } from "./dot-prop-paths";
-import safeKeyValue, { PrimaryKeyValue, makePrimaryKeyGetter } from "./getKeyValue";
+import safeKeyValue, { PrimaryKeyGetter, PrimaryKeyValue, makePrimaryKeyGetter } from "./getKeyValue";
 import isPlainObject from "./isPlainObject";
 import { WhereFilter, WhereFilterDefinition } from "./where-filter";
 import { WriteAction, WriteActionPayload, WriteActions } from "./write-actions";
 
 export {isPlainObject, safeKeyValue, makePrimaryKeyGetter};
-export type {PrimaryKeyValue}
+export type {PrimaryKeyValue, PrimaryKeyGetter}
 
 export {WhereFilter};
 export type {WhereFilterDefinition};

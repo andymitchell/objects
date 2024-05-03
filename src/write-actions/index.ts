0446788124ca307041253b8de37b8b9b08c86e83
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-05-03 13:50:57 +0100

    Export createWriteActionSuccessesSchema

--- src/write-actions/index.ts
+++ src/write-actions/index.ts
@@ -1,11 +1,12 @@
 import { applyWritesToItems, DDL } from "./applyWritesToItems/index";
 import combineWriteActionsWhereFilters from "./combineWriteActionsWhereFilters";
-import {  WriteActionAffectedItem, WriteActionFailureAffectedItem, AppliedWritesOutput, WriteAction, WriteActionError, WriteActionErrorSchema, WriteActionFailures, WriteActionPayload, assertArrayScope, createWriteActionFailuresSchema, createWriteActionSchema, WriteActionFailuresErrorDetails, WriteActionSuccesses } from "./types";
+import {  WriteActionAffectedItem, WriteActionFailureAffectedItem, AppliedWritesOutput, WriteAction, WriteActionError, WriteActionErrorSchema, WriteActionFailures, WriteActionPayload, assertArrayScope, createWriteActionFailuresSchema, createWriteActionSchema, WriteActionFailuresErrorDetails, WriteActionSuccesses, createWriteActionSuccessesSchema } from "./types";
 
 export const WriteActions = {
     applyWritesToItems,
     combineWriteActionsWhereFilters,
     createWriteActionSchema,
+    createWriteActionSuccessesSchema,
     createWriteActionFailuresSchema,
     WriteActionErrorSchema,
     assertArrayScope

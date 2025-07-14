95a1c91bd181f64205c623640b67fa06957c978b
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2025-07-14 19:16:07 +0100

    Add roadmap tests

--- src/where-filter/types.test.ts
+++ src/where-filter/types.test.ts
@@ -48,23 +48,109 @@ it('it correctly identifies the available dot prop sub keys, and their type, but
 
 })
 
+describe('TODO - desirable features', () => {
 
-it('should try to control types even on permissive records', () => {
-    type ErrorObject = {[x: string]: any;} & {
-        name?: string;
-        message?: string;
-        stack?: string;
-        cause?: unknown;
-        code?: string;
-    }
+    /**
+     * For performance: 
+     * - Typescript 7 is coming in Go, with a 10x speed up. 
+     * - We could compile a '*Filterable' version of every type to use in a WhereFilterCompiledDefinition, and have a script that auto-compiles those types.
+     * 
+     * Ideally we'd do precompiling now, with an easy way to switch out later (by text-replacing naming conventions perhaps).
+     */
 
-    // TODO
-    // Sadly this currently doesn't work. 
-    // In an ideal world, it detect that "message" has to be a string. But typescript flips into full permissive mode. 
-    const a: WhereFilterDefinition<ErrorObject> = {
-        'message': 1 // Expect this to fail because of in ErrorObject 'message' is a string. But it doesn't fail. It doesn't have any opinion on any key.
-    }
+    it('handles a variable of the same type', () => {
+
+        type MessagingError = NoListenerError | TimeOutError;
+
+        type MessagingErrorLocation = 'sendMessage' | 'onMessage';
+        type Message = {};
+
+        interface BaseMessagingError {
+            type: string,
+            location: MessagingErrorLocation;
+            description: string;
+            message: Message;
+        }
+
+        interface NoListenerError extends BaseMessagingError {
+            type: 'missing-or-stolen-listener';
+        }
+
+        interface TimeOutError extends Omit<BaseMessagingError, 'message'> {
+            type: 'time-out';
+            message?: Message
+        }
+
+        function test(type: keyof MessagingError) {
+            const b: WhereFilterDefinition<{error: MessagingError}> = {
+                // @ts-ignore The types are the same. It should work
+                'error.type': type
+                //'error.type': 'missing-or-stolen-listener' // DOing it inline does work
+            }
+
+        }
+
+
+    })
+
+    it('handles top level array', () => {
+
+        type Obj = {name: string};
+        const b: WhereFilterDefinition<Obj[]> = {
+            // It finds no dot prop: but I'd expect a top level elem_match on it 
+        }
+
+    })
+
+    it('handles an object or array union', () => {
+        type Obj = {name: string};
+        const b: WhereFilterDefinition<{objects: Obj | Obj[]}> = {
+            // Finds no dot-prop, but I'd expect it offer both object or elem_match for array. And to handle both. 
+        }
+    })
+
+    it('treat nested objects are partial matches, not deepEql', () => {
+        type Obj = {name: string, sibling: {name: string, age: number}};
+
+        // Currently this is how to do sibling.name: 
+        const a: WhereFilterDefinition<Obj> = {
+            'sibling.name': 'Bob'
+        }
+
+        // But in regular use I found myself attempting this and being confused about why it doesn't let me do partials. (It's because its a deepEql comparison requiring the full object beyond the top level).
+        const b: WhereFilterDefinition<Obj> = {
+            // @ts-expect-error Oh it does actually throw an error for not using 'age'. It's perhaps more a mental model thing. 
+            // I.e. that WhereFilterDefinition is only dot-prop keys, not partial nesting. 
+            // But maybe Mongo allows it to be more explicit? 
+            'sibling': {
+                'name': 'Bob',
+                //'age': 1
+            }
+        }
+
+        // I think in Mongo the default is it would work. And if you want to match a full object you provide everything. We should mimic that. 
+
+
+    })
+
+    it('should try to control types even on permissive records', () => {
+        type ErrorObject = {[x: string]: any;} & {
+            name?: string;
+            message?: string;
+            stack?: string;
+            cause?: unknown;
+            code?: string;
+        }
+
+        // TODO
+        // Sadly this currently doesn't work. 
+        // In an ideal world, it detect that "message" has to be a string. But typescript flips into full permissive mode. 
+        const a: WhereFilterDefinition<ErrorObject> = {
+            'message': 1 // Expect this to fail because of in ErrorObject 'message' is a string. But it doesn't fail. It doesn't have any opinion on any key.
+        }
+    })
 })
+
 
 it('it throws a type error if using an unknown key', () => {
 

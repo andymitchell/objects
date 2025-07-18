e93195fbd3f962b96aa427d6232dc33217b9cb42
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2025-07-18 13:48:27 +0100

    Dep update + some notes that were missed in the last save

--- src/where-filter/performance-experiments/full-compilation/compiler/gemini/filters-auto.ts
+++ src/where-filter/performance-experiments/full-compilation/compiler/gemini/filters-auto.ts
@@ -2,3 +2,4 @@ import type { User } from './user.ts'
 import type {WhereFilterDefinition} from './wherefilter.ts'
 
 export type UserFilter = WhereFilterDefinition<User> // Resolves to: {name: string, 'address.city': string, 'address.zip': string}
+

f61a35543855300e97037a4474eaafc3620d59dd
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2026-03-06 08:37:29 +0000

    Fix type errors

--- src/write-actions/writeToItemsArray/helpers/getArrayScopeItemAction.ts
+++ src/write-actions/writeToItemsArray/helpers/getArrayScopeItemAction.ts
@@ -1,43 +1,45 @@
 import { z } from "zod";
 import { getPropertySpreadingArrays } from "../../../dot-prop-paths/getPropertySimpleDot.ts";
-import type { DotPropPathValidArrayValue } from "../../../dot-prop-paths/types.ts";
 import { getZodSchemaAtSchemaDotPropPath } from "../../../dot-prop-paths/zod.ts";
 import type { WriteAction } from "../../types.ts";
 import { isWriteActionArrayScopePayload } from "../../helpers.ts";
 import type { DDL } from "../types.ts";
+import type { DotPropPathValidArrayValue } from "../../../dot-prop-paths/types.ts";
 
 
 type ArrayScopeSchemaAndDDL<ScopedType extends Record<string, any>> = {writeAction:WriteAction<ScopedType>, schema: z.ZodType<ScopedType, any, any>, ddl: DDL<ScopedType>};
 type Scoped<ScopedType extends Record<string, any>> = ArrayScopeSchemaAndDDL<ScopedType> & {items: ScopedType[], path: string};
 export function getArrayScopeSchemaAndDDL<T extends Record<string, any>>(writeAction:Readonly<WriteAction<T>>, schema: z.ZodType<T, any, any>, rules:DDL<T>) {
     const payload = writeAction.payload;
-    if( !isWriteActionArrayScopePayload(payload) ) throw new Error("Expects Array Scope Write Action");
+    if( !isWriteActionArrayScopePayload<T>(payload) ) throw new Error("Expects Array Scope Write Action");
+    const scope: string = payload.scope;
+    const action = payload.action;
     
-    type ScopedType = DotPropPathValidArrayValue<T, typeof payload.scope>; // Note that because of generics, this type is meaningless to the type checker. Helpful to read though. 
+    type ScopedType = DotPropPathValidArrayValue<T, typeof payload.scope>;
 
     type ScopedListRules = Partial<DDL<ScopedType>['lists']>;
     const scopedListRules:ScopedListRules = {};
     let ruleKey: keyof typeof rules.lists;
     for( ruleKey in rules.lists ) {
-        if( ruleKey.indexOf(payload.scope)===0 ) {
-            const scopedRuleKey = ruleKey===payload.scope? '.' : ruleKey.replace(payload.scope, '') as keyof DDL<ScopedType>['lists'];
-            
-            // @ts-ignore this is solvable, it's just being a pain 
+        if( ruleKey.indexOf(scope)===0 ) {
+            const scopedRuleKey = ruleKey===scope? '.' : ruleKey.replace(scope, '') as keyof DDL<ScopedType>['lists'];
+
+            // @ts-ignore this is solvable, it's just being a pain
             scopedListRules[scopedRuleKey] = rules.lists[ruleKey];
         }
     }
     if( Object.keys(scopedListRules).length===0 ) debugger;
 
-    const scopedSchema = getZodSchemaAtSchemaDotPropPath(schema, payload.scope);
+    const scopedSchema = getZodSchemaAtSchemaDotPropPath(schema, scope);
     if( !scopedSchema ) throw new Error("Could not scope the schema. Suggests the schema and the dot-prop-path don't align.");
 
     // expand payload into an action
-    const scopedWriteAction:WriteAction<ScopedType> = {
-        type: 'write',
+    const scopedWriteAction = {
+        type: 'write' as const,
         ts: writeAction.ts,
-        uuid: writeAction.uuid+payload.scope,
-        payload: payload.action
-    }
+        uuid: writeAction.uuid+scope,
+        payload: action
+    } as WriteAction<ScopedType>;
 
     const output: ArrayScopeSchemaAndDDL<ScopedType> = {
         writeAction: scopedWriteAction,
@@ -49,14 +51,16 @@ export function getArrayScopeSchemaAndDDL<T extends Record<string, any>>(writeAc
 
 export default function getArrayScopeItemAction<T extends Record<string, any>>(item:T, writeAction:Readonly<WriteAction<T>>, schema: z.ZodType<T, any, any>, rules:DDL<T>) {
     const payload = writeAction.payload;
-    if( !isWriteActionArrayScopePayload(payload) ) throw new Error("Expects Array Scope Write Action");
+    if( !isWriteActionArrayScopePayload<T>(payload) ) throw new Error("Expects Array Scope Write Action");
+    const scope: string = payload.scope;
 
     const scopedSchemaAndDDL = getArrayScopeSchemaAndDDL<T>(writeAction, schema, rules);
 
-    type ScopedType = DotPropPathValidArrayValue<T, typeof payload.scope>; // Note that because of generics, this type is meaningless to the type checker. Helpful to read though. 
+    
+    type ScopedType = DotPropPathValidArrayValue<T, typeof payload.scope>;
 
 
-    const propertyResults = getPropertySpreadingArrays(item, payload.scope);
+    const propertyResults = getPropertySpreadingArrays(item, scope);
 
 
     return propertyResults.map(scopedItems => {
